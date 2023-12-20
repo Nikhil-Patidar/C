907b9d414c72e7e48c0e4570f1924fd3df080ff5
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2023-12-20 23:01:02 +0530

    Add files via upload

---
+++ if_ else.c
@@ -0,0 +1,50 @@
+
+			//wap to check whether an age is eligible to vote or not
+
+#include<stdio.h>
+int main()
+{
+/*	int age;
+	printf("Enter the age\n");
+	scanf("%d",&age);
+	if(age>=18)
+	{
+		printf("\n eligibler for voting");
+	}
+	else
+	printf("\n not eligible for voting");
+	return 0;*/
+
+				//WAP A PROGRAM TO FIND GREATER NUMBER BETWEEN TWO NUMBERS
+				
+				
+/*	int a,b;
+	printf("enter the two numbers\n");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		printf("max number=%d\n",a);
+	}
+	else
+	
+	printf("max number=%d\n",b);	*/
+							
+	
+			// WAP A PROGRAM TO FIND GREATER THREE NUMBERS
+	
+		int a,b,c;
+		printf(" Enter the three numbers\n");
+		scanf("%d%d%d",&a,&b,&c);
+		if((a>b) && (a>c))
+		{
+			printf("max number a=%d\n",a);
+		}
+		else if((b>a) && (b>c))
+		{
+			printf("max number b=%d\n",b);
+		}
+	
+		
+		else
+		printf("max number c=%d\n",c);
+}
