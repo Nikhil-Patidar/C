6645efda9554f7d431d98ba17ece37457de9e598
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2023-12-28 18:55:41 +0530

    Add files via upload

---
+++ star pattern.c
@@ -0,0 +1,25 @@
+/*#include<stdio.h>
+
+int main()
+{
+    int i,j,n;
+    i=1;
+     
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        j=1;
+        printf("\n");
+        while(j<=i)
+        {
+            printf("*");
+            j++;
+        }
+        i++;
+    }
+    return 0;
+} */
+
+
+
