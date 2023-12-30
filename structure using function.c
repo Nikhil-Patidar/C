6a04c5acf38100142482df134d78933d2017c3c7
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2023-12-30 23:10:54 +0530

    Add files via upload

---
+++ structure using function.c
@@ -0,0 +1,31 @@
+
+// wap to  enployee structure read value and display value
+// structure as fuction argument
+#include<stdio.h>
+struct employee
+{
+    int code;
+    char name[10];
+    float salary;
+};
+void display(struct employee); // fuction prototype 
+int main()
+{
+    struct employee a;// variable declaration struct type
+    // read value structure
+    printf("\nENTER EMPLOYEE DETAILS");
+    printf("\nENTER EMPLOYEE CODE");
+    scanf("%d",&a.code);
+    printf("\nENTER EMPLOYEE NAME");
+    scanf("%d",&a.name);
+    printf("\nENTER EMPLOYEE SALARY");
+    scanf("%f",&a.salary);
+    display(a);
+    return 0;
+}
+void display(struct employee x) // copy of variabe aa to x
+{
+    printf("CODE=%d",x.code);
+    printf("NAME=%s",x.name);
+    printf("salary=%f",x.salary);
+}
