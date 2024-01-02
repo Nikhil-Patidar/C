1da3066b10634838c1c09bbb9eb1bfe28b4cc898
Nikhil-Patidar <153012307+Nikhil-Patidar@users.noreply.github.com>
2024-01-02 18:56:57 +0530

    Add files via upload

---
+++ switch .c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+	int ch;
+	printf("Enter the any number 1-7\n");
+	scanf("%d",&ch);
+	switch(ch)
+	{
+		case 1:
+			printf("\n sunday");
+			break;
+		case 2:
+			printf("\n monday");
+			break;
+		case 3:
+			printf("\n tuesday");
+			break;	
+		case 4:
+			printf("\nwednesday");
+			break;
+		case 5:
+			printf("\n thursday");
+			break;	
+		case 6:
+			printf("\n friday");
+			break;	
+		case 7:
+			printf("\n saturday");
+			break;
+		default:
+			printf("\n wrong choice");
+			break;
+	}
+	
+	return 0;
+
+}
